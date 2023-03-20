72fb3b2ccee3c51bea7fcd9356e2d7d394148c9c
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2023-03-20 12:29:28 +0200

    linkitykset tapahtuma-sivulle ja etusivulle

--- src/view/template.php
+++ src/view/template.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <header>
-      <h1>Junnukoodarit - koulutuskalenteri</h1>
+    <h1><a href="<?=BASEURL?>">Junnukoodarit - Tapahtumakalenteri</a></h1>
     </header>
     <section>
       <?=$this->section('content')?>

3ea3f4f0ad5d5be96d55f4e71f51dcac613f6345
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2023-03-21 15:53:10 +0200

    uloskirjautuminen

--- public/index.php
+++ public/index.php
@@ -64,6 +64,12 @@ require_once '../src/init.php';
       echo $templates->render('kirjaudu', [ 'error' => []]);
     }
     break;
+    
+    case "/logout":
+      require_once CONTROLLER_DIR . 'kirjaudu.php';
+      logout();
+      header("Location: " . $config['urls']['baseUrl']);
+      break;
 
 
     

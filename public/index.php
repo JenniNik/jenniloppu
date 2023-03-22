c8aa29323c30eee532ee308a9092657e0d827f4c
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2023-03-22 14:53:39 +0200

    vahvistuslinkin aktivointi

--- public/index.php
+++ public/index.php
@@ -118,7 +118,20 @@ require_once '../src/init.php';
             header("Location: tapahtumat");  
           }
           break;
-    
+          case "/vahvista":
+            if (isset($_GET['key'])) {
+              $key = $_GET['key'];
+              require_once MODEL_DIR . 'henkilo.php';
+              if (vahvistaTili($key)) {
+                echo $templates->render('tili_aktivoitu');
+              } else {
+                echo $templates->render('tili_aktivointi_virhe');
+              }
+            } else {
+              header("Location: " . $config['urls']['baseUrl']);
+            }
+            break;
+      
     
     default:
       echo $templates->render('notfound');

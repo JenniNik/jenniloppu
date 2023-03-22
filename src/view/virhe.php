5e18ca9aa4e07a986c9a4c49a376a5e3dc1b13a7
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2023-03-22 15:57:36 +0200

    vaihtoavaimen lähettäminen

---
+++ src/view/virhe.php
@@ -0,0 +1,6 @@
+<?php $this->layout('template', ['title' => 'Hups, meidän moka..']) ?>
+
+<h1>Hups, meidän moka..</h1>
+
+<p>Vaikuttaa siltä, että meidän täytyy vähän skarpata ja laittaa homma rullaamaan uudelleen. 
+   Ole hyvä ja kokeile pienen hetken päästä onnistuimmeko siinä.</p>
